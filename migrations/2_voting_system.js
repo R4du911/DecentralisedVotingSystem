a88b4cd2788cefe2c6207e4c49c149420d0a51c1
Radu Siderias <radusiderias@yahoo.com>
2024-05-15 14:02:17 +0300

    trying to fix frontend connection

--- migrations/2_voting_system.js
+++ migrations/2_voting_system.js
@@ -1,5 +1,5 @@
 const VotingSystem = artifacts.require("VotingSystem");
 
-module.exports = function (deployer) {
-  deployer.deploy(VotingSystem);
+module.exports = function (deployer, network, accounts) {
+  deployer.deploy(VotingSystem, { from: accounts[0] });
 };
